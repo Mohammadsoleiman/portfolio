4054fb839932a856ea7ed073b54bdca36d8774df
Mohammad Suleiman <mohammadsoleiman6@gmail.com>
2026-01-03 18:44:29 +0200

    update portfolio final version

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// ✅ Auto-detect base path
 export default defineConfig({
   plugins: [react()],
-  // base: process.env.NODE_ENV === "production" ? "/portfolio/" : "/", // 👈 ensures CSS & JS load correctly on GitHub Pages
   build: {
     outDir: "dist",
     assetsDir: "assets",
@@ -15,6 +13,3 @@ export default defineConfig({
     open: true,
   },
 });
-
-
-// https://mohammadsoleiman.github.io/portfolio/
