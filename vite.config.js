1d79cc6b41d1dd607f7e19300f2b6dc0d6cd1fb5
Mohammad Suleiman <mohammadsoleiman6@gmail.com>
2026-01-03 19:56:39 +0200

    update portfolio final version

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: 'public/img', dest: '' },
+        { src: 'public/icons', dest: '' },
+        { src: 'public/MohammadSoleimanCV.pdf', dest: '' }
+      ]
+    })
+  ],
   base: "/",
 });
