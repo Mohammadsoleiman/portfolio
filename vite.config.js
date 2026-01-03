c5b7a442d808917fa69bfcee6cb3d9eb4a5bab2f
Mohammad Suleiman <mohammadsoleiman6@gmail.com>
2026-01-03 18:49:16 +0200

    update portfolio final version

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,17 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  // IMPORTANT: because your site is hosted at
+  // https://mohammadsoleiman.github.io/portfolio/
+  base: "/portfolio/",
+
   build: {
     outDir: "dist",
     assetsDir: "assets",
     sourcemap: false,
   },
+
   server: {
     port: 5173,
     open: true,
